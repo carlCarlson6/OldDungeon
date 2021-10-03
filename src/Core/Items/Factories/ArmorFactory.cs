a0f98a898ff798445271b0a06e6b845e04f022a4
Carlos Acitores Deval <carlos.acitores@colba.es>
2021-10-04 01:00:11 +0200

    minor changes

--- src/Core/Items/Factories/ArmorFactory.cs
+++ src/Core/Items/Factories/ArmorFactory.cs
@@ -2,17 +2,13 @@ namespace Core.Items.Factories
 {
     public static class ArmorFactory
     {
-        public static Protection CreateLeatherArmor() => 
-            new ("Leather armor", ItemType.Armour, 2);
+        public static Protection CreateLeatherArmor() => new ("Leather armor", ItemType.Armour, 2);
         
-        public static Protection CreateRingsArmor() => 
-            new ("Rings armor", ItemType.Armour, 3);
+        public static Protection CreateRingsArmor() => new ("Rings armor", ItemType.Armour, 3);
         
-        public static Protection CreateChainMailArmor() => 
-            new ("Chain mail armor", ItemType.Armour, 4);
+        public static Protection CreateChainMailArmor() => new ("Chain mail armor", ItemType.Armour, 4);
         
-        public static Protection CreatePlateArmor() => 
-            new ("Plate armor", ItemType.Armour, 6);
+        public static Protection CreatePlateArmor() => new ("Plate armor", ItemType.Armour, 6);
         
     }
 }
--- src/Core/Items/Factories/ShieldFactory.cs
+++ src/Core/Items/Factories/ShieldFactory.cs
@@ -2,7 +2,8 @@ namespace Core.Items.Factories
 {
     public static class ShieldFactory
     {
-        public static Protection CreateWoodenShield() =>
-            new ("Wooden shield", ItemType.Shield, 3);
+        public static Protection CreateWoodenShield() => new ("Wooden shield", ItemType.Shield, 1);
+        
+        public static Protection CreateIronShield() => new ("Iron shield", ItemType.Shield, 2);
     }
 }
