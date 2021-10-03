7467e0688dfe427a8f4557e88004003798736e25
Carlos Acitores Deval <carlos.acitores@colba.es>
2021-10-03 23:12:14 +0200

    more potion types

--- src/Core/Items/Factories/ConsumableFactory.cs
+++ src/Core/Items/Factories/ConsumableFactory.cs
@@ -4,15 +4,41 @@ namespace Core.Items.Factories
 {
     public static class ConsumableFactory
     {
+        public static Consumable CreateSmallHealthPotion()
+        {
+            var consumableApplier = new Func<Hero, Result>((Hero hero) =>
+            {
+                var pointsToCure = Dice.Throw(1,6);
+                hero.Cure(pointsToCure);
+
+                // TODO - set correct info
+                return new Result();
+            });
+            return new Consumable("Health potion", consumableApplier);
+        }
+        
         public static Consumable CreateHealthPotion()
         {
             var consumableApplier = new Func<Hero, Result>((Hero hero) =>
             {
-                var pointsToCure = Dice.WithFaces(4).Throw(2);
+                var pointsToCure = Dice.Throw(2,4);
                 hero.Cure(pointsToCure);
 
-                //return new ActionResult();
-                throw new NotImplementedException();
+                // TODO - set correct info
+                return new Result();
+            });
+            return new Consumable("Health potion", consumableApplier);
+        }
+        
+        public static Consumable CreateLargeHealthPotion()
+        {
+            var consumableApplier = new Func<Hero, Result>((Hero hero) =>
+            {
+                var pointsToCure = Dice.Throw(2,4,2);
+                hero.Cure(pointsToCure);
+
+                // TODO - set correct info
+                return new Result();
             });
             return new Consumable("Health potion", consumableApplier);
         }
