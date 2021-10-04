d947b07903e6092f898340591f00483851a4d0d9
Carlos Acitores Deval <carlos.acitores@colba.es>
2021-10-04 14:55:33 +0200

    StringExtensions.cs

---
+++ src/Core/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static int ToInt32(this string? target) =>
+            Convert.ToInt32(target);
+    }
+}
